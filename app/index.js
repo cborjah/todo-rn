5878a8d48a3d9bf3a23b68bf8c45586364de83c7
Christian Borja <cpborja32@gmail.com>
2017-08-15 14:50:16 -0700

    sets up eslint and begins login screen

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+import Login from './screens/Login';
+
+EStyleSheet.build({
+  // Shows borders for all visible components
+  // outline: 1,
+});
+
+export default () => <Login />;
